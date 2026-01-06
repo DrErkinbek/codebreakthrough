6666e7047e34858f57d1dce0b12cf0e72b67e87d
Erkinbek <erkinbekabdurakhmanov@gmail.com>
2026-01-05 20:53:35 -0600

    working with typescript

---
+++ backend/api-ts/source/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+	res.send('Hellowords')
+});
+
+
+app.listen(port, () => {
+	console.log(`http://localhost:${port}`);
+});
+
